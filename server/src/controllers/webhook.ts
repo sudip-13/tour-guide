fcacb53ce1e1ee9e89ac76ab35db5c4ffd088cd4
SUDIP KUNDU <sudip2003kundu@gmail.com>
2024-08-08 21:52:36 +0530

    on the way chat bot

--- server/src/controllers/webhook.ts
+++ server/src/controllers/webhook.ts
@@ -1,4 +1,4 @@
-import { Request,Response } from 'express';
+import { Request, Response } from 'express';
 import stationData from '../../data/stationcode.json';
 import { Parameters } from '../routes/webhook';
 import axios from 'axios';
@@ -11,6 +11,7 @@ interface Station {
 interface StationData {
     data: Station[];
 }
+
 interface TrainBase {
     train_name: string;
     train_no: string;
@@ -34,106 +35,64 @@ interface TrainApiResponse {
     data: TrainData[];
 }
 
-// Define the structure of the Dialogflow request
-interface DialogflowRequest {
-    queryResult: {
-        parameters: {
-            Station: string[];
-        };
-    };
-}
-
-// Define the structure of the response object
-interface FulfillmentMessage {
-    card?: {
-        title: string;
-        subtitle: string;
-        formattedText: string;
-        buttons: {
-            text: string;
-            postback: string;
-        }[];
-    };
-    text?: {
-        text: string[];
-    };
-}
-
-interface FulfillmentResponse {
-    fulfillmentMessages: FulfillmentMessage[];
-}
 function getStationCode(stationName: string, stationData: StationData): string | null {
     const station = stationData.data.find(station => station.name.toLowerCase() === stationName.toLowerCase());
     return station ? station.code : null;
 }
 
 export async function findTrains(parameters: Parameters): Promise<any> {
-
-    // const dfMessenger = document.querySelector('df-messenger');
-    // const payload = [
-    //   {
-    //     "type": "info",
-    //     "title": "Info item title",
-    //     "subtitle": "Info item subtitle",
-    //     "image": {
-    //       "src": {
-    //         "rawUrl": "https://example.com/images/logo.png"
-    //       }
-    //     },
-    //     "actionLink": "https://example.com"
-    //   }];
-    // dfMessenger?.renderCustomCard(payload);
-
-
     try {
-
         const sourceStationName = parameters.Station[0];
         const destinationStationName = parameters.Station[1];
-
-  
         const sourceCode = getStationCode(sourceStationName, stationData);
         const destinationCode = getStationCode(destinationStationName, stationData);
 
         if (sourceCode && destinationCode) {
-   
             const response = await axios.get<TrainApiResponse>(`${process.env.SERVER_URL}/trains/betweenStations/?from=${sourceCode}&to=${destinationCode}`);
             const trainDataArray = response.data.data;
 
             if (trainDataArray.length > 0) {
-     
-                const limitedTrainDataArray = trainDataArray.slice(0, 10);
-
-     
-                const fulfillmentMessages: FulfillmentMessage[] = limitedTrainDataArray.map((trainDataObj: TrainData) => {
+                const res = trainDataArray.map((trainDataObj: TrainData) => {
                     const trainData = trainDataObj.train_base;
-                    return {
-                        card: {
-                            title: `${trainData.train_name} (${trainData.train_no})`,
-                            subtitle: `From ${trainData.from_stn_name} to ${trainData.to_stn_name}`,
-                            formattedText: `
-                                Source: ${trainData.source_stn_name} (${trainData.source_stn_code})\n
-                                Destination: ${trainData.dstn_stn_name} (${trainData.dstn_stn_code})\n
-                                Departure: ${trainData.from_time}\n
-                                Arrival: ${trainData.to_time}\n
-                                Travel Time: ${trainData.travel_time}\n
-                                Running Days: ${trainData.running_days}
-                            `,
-                            buttons: [
-                                {
-                                    text: "Book Now",
-                                    postback: "https://www.irctc.co.in/nget/train-search"
-                                }
-                            ]
-                        }
-                    };
-                });
 
-                return{
-                    fulfillmentMessages
-                }
-                
+                    [
+                        {
+                            'text': {
+                                'text': [
+                                    'Sample text'
+                                ]
+                            }
+                        },
+                        {
+                            'payload': {
+                                'richContent': [
+                                    {
+                                        "type": "accordion",
+                                        "title": `${trainData.train_name} (${trainData.train_no})`,
+                                        "subtitle": `From ${trainData.from_stn_name} to ${trainData.to_stn_name}`,
+                                        "image": {
+                                            "src": {
+                                                "rawUrl": "https://example.com/images/logo.png"
+                                            }
+                                        },
+                                        "text": `
+                                        Source: ${trainData.source_stn_name} (${trainData.source_stn_code})\n
+                                        Destination: ${trainData.dstn_stn_name} (${trainData.dstn_stn_code})\n
+                                        Departure: ${trainData.from_time}\n
+                                        Arrival: ${trainData.to_time}\n
+                                    `
+                                    }
+                                ]
+                            }
+                        }
+                    ]
+
+
+                });
+                return res
+
             } else {
-                Response.json({
+                return {
                     fulfillmentMessages: [
                         {
                             text: {
@@ -143,10 +102,10 @@ export async function findTrains(parameters: Parameters): Promise<any> {
                             }
                         }
                     ]
-                });
+                };
             }
         } else {
-            Response.json({
+            return {
                 fulfillmentMessages: [
                     {
                         text: {
@@ -156,12 +115,12 @@ export async function findTrains(parameters: Parameters): Promise<any> {
                         }
                     }
                 ]
-            });
+            };
         }
     } catch (err) {
-        console.error(err);
+        console.error('Error occurred while fetching train data:', err);
 
-        Response.json({
+        return {
             fulfillmentMessages: [
                 {
                     text: {
@@ -171,6 +130,6 @@ export async function findTrains(parameters: Parameters): Promise<any> {
                     }
                 }
             ]
-        });
+        };
     }
 }
